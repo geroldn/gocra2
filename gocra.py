cf3b557df864ce071ad214e9f61b9c13b29a3139
geroldn <gerold@nicolasen.com>
2019-12-23 23:06:37 +0100

    Initializing added for rating and pairing in series

--- gocra.py
+++ gocra.py
@@ -7,6 +7,7 @@ class Settings:
     s_home = '/Users/gerold/dev/python/gocra/'
     s_tfile = 'UGC-2019-1.xml'
     s_name_column_width = 32
+    s_ronde_column_width = 12
 
 class Serie:
     def __init__(self, gocra):
@@ -21,24 +22,60 @@ class Serie:
 
     def treg(self):
         self.numberOfRounds = int(self.doc['Tournament']['NumberOfRounds'])
+        self.currentRoundNumber = int(self.doc['Tournament']['CurrentRoundNumber'])
+        self.takeCurrentRoundInAccount = self.doc['Tournament']['TakeCurrentRoundInAccount']
         self.participants = []
+        self.pairings = []
+        pNr = 0
         for p in self.doc['Tournament']['IndividualParticipant']:
             if p['PreliminaryRegistration'] == 'false':
-                self.participants.append(Participant(
+                sParticipant = (Participant(
                       p['GoPlayer']['Surname'] + ', '
                     + p['GoPlayer']['FirstName']
                     , p['GoPlayer']['GoLevel']
+                    , int(p['GoPlayer']['Rating'])
                     , p['Id']))
+                self.participants.append(sParticipant)
+                self.pairings.append([])
+                for r in range(self.currentRoundNumber):
+                    self.pairings[pNr].append({})
+                    self.regPairing(pNr, r)
+                pNr = pNr + 1
+        self.setNr()
+        self.setStartRatings()
+        self.calcResultRatings()
+
+    def setNr(self):
+        for n, p in enumerate(self.participants, start=1):
+            p.setNr(n)
+
+    def setStartRatings(self):
+        for p in self.participants:
+            p.startRating = p.rating
+
+    def calcResultRatings(self):
+        for p in self.participants:
+            p.resultRating = p.startRating
+
+    def regPairing(self, nParticipant, nRound):
+        self.pairings[nParticipant][nRound]['string'] = '   -'
 
     def print(self):
-        w = Settings.s_name_column_width
-        pstr = '+' + '+'.rjust(w, '-')
+        nw = Settings.s_name_column_width
+        rw = Settings.s_ronde_column_width
+        line1 = '+' + '+'.rjust(nw, '-')
         for i in range(self.numberOfRounds):
-            pstr = pstr + (' Ronde ' + str(i+1) + ' ').ljust(12, '-') + '+'
-        print(pstr)
-        for p in self.participants:
-            print(('|' + p.name + ' ('+ p.strength + ')').ljust(w) + '|')
-        print(pstr)
+            line1 = line1 + (' Ronde ' + str(i+1) + ' ').ljust(rw , '-') + '+'
+        print(line1)
+        for n, p in enumerate(self.participants):
+            line2 = ('|' + str(p.nr).rjust(2) + ' ' + p.name + ' ('+ p.strength + ')').ljust(nw) + '|'
+            for i in range(self.currentRoundNumber):
+                line2 = line2 + self.pairings[n][i]['string'].ljust(rw) + '|'
+            for i in range(self.numberOfRounds - self.currentRoundNumber):
+                line2 = line2 + ' '.ljust(rw) + '|'
+            line2 = line2 + str(p.startRating).rjust(4) + ' - ' + str(p.resultRating).rjust(4)
+            print(line2)
+        print(line1)
 
 class Rating:
     def __init__(self, date, rating):
@@ -48,20 +85,17 @@ class Rating:
                 }
 
 class Participant:
-    def __init__(self, name, strength, _id):
+    def __init__(self, name, strength, rating, _id):
         self.name = name
         self.strength = strength
+        self.rating = rating
         self._id = _id
-        self.ratings = []
+        self.startRating = 0
+        self.resultRating = 0
+        self.pairings = []
 
-    def addRating(self, date, rating):
-        self.ratings.append(Rating(date, rating))
-
-    def print(self):
-        s = self.name.ljust(20)
-        for r in self.ratings:
-            s = s + r.rating["date"].ljust(9) + str(r.rating["rating"]).ljust(5)
-        print(s)
+    def setNr(self, nr):
+        self.nr = nr
 
 class Ratinglist:
     def __init__(self):
@@ -95,6 +129,8 @@ class Gocra:
             print(k)
         for k in self.serie.doc['Tournament']['IndividualParticipant']:
             print(k)
+        for r in self.serie.doc['Tournament']['TournamentRound']:
+            print(r)
         print('Number of rounds: ' + str(self.serie.numberOfRounds))
         self.serie.print()
 
