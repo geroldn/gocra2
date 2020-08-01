2c35269e2416543a218a83dbe00a29339801848e
geroldn <gerold@nicolasen.com>
2020-08-01 14:41:40 +0200

    Start on file upload (for macmahon)

--- wgocra/urls.py
+++ wgocra/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('current/', views.current, name='gocra-current'),
     path('clubs/', views.ClubListView.as_view(), name='gocra-clubs'),
     path('players/', views.PlayerListView.as_view(), name='gocra-players'),
+    path('series/', views.series, name='gocra-series'),
+    path('upload/', views.upload_file, name='gocra-upload'),
 ]
--- wgocra/views.py
+++ wgocra/views.py
@@ -2,11 +2,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from .forms import UploadFileForm
+
+import logging
+logging.basicConfig(filename='log/gocra.log', level=logging.DEBUG)
 
 from wgocra.models import Club, Player
 
 def index(request):
     return render(request, 'wgocra/index.html')
+
+def series(request):
+    return render(request, 'wgocra/series.html')
 
 def current(request):
     return render(request, 'wgocra/current.html')
@@ -19,3 +26,17 @@ class ClubListView(ListView):
 class PlayerListView(ListView):
     model = Player
     template_name = 'wgocra/player_list.htl'
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        logging.debug(request.FILES['file'])
+        if form.is_valid():
+            logging.debug('POST valid')
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('wgocra/series.html')
+        logging.debug('POST not valid')
+    else:
+        logging.debug('not POST')
+        form = UploadFileForm()
+    return render(request, 'wgocra/upload.html', {'form': form})
